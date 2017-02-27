a95806d15f76cd9e24db55beb3b44192af6a2dd1
Devin <itsnotafake@gmail.com>
2017-02-27 14:14:25 -0800

    doc
    Added further description to the Architecture text file

---
+++ Atakr/app/src/main/java/templar/atakr/MainActivity.java
@@ -0,0 +1,18 @@
+package templar.atakr;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Devin on 2/20/2017.
+ */
+
+public class MainActivity extends FragmentActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}C
