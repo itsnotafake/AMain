fa68f69cd94bd4a303fe4513faf7155afc6f61f4
Devin <itsnotafake@gmail.com>
2017-03-03 15:38:45 -0800

    feat
    Added authentication (although there is no way to sign out and FirebaseUI's smart lock thing isn't working which isnt much of a shocker) and added the new logo

--- Atakr/app/src/main/java/templar/atakr/MainActivity.java
+++ Atakr/app/src/main/java/templar/atakr/MainActivity.java
@@ -1,7 +1,14 @@
 package templar.atakr;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 /**
  * Created by Devin on 2/20/2017.
@@ -10,9 +17,55 @@ import android.support.v4.app.FragmentActivity;
 public class MainActivity extends FragmentActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private static final int RC_SIGN_IN = 111;
+
+    //Firebase related variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        //Firebase Variable initialization
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        initializeAuthStateListener();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    //Initializes our Authentication State Listener
+    private void initializeAuthStateListener(){
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    //TODO
+                    //user is signed in
+                }else{
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .setTheme(R.style.AppTheme)
+                                    .setIsSmartLockEnabled(false)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
     }
 }
